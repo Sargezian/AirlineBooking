5d5748ab0152df1eb621f132959076cad61e7085
BuilderMARK <73698747+BuilderMARK@users.noreply.github.com>
2021-05-23 17:41:15 +0200

    Testpånogle forskelligeviews

--- test/client/model/SaveInfoTest.java
+++ test/client/model/SaveInfoTest.java
@@ -4,41 +4,35 @@ import org.junit.Test;
 import shared.transferobjects.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SaveInfoTest {
+public class SaveInfoTest
+{
 
     @Test // vi har testet singleton//
-    public void testSingletonIfNull(){
+    public void testSingletonIfNull()
+    {
 
         SaveInfo saveInfo1 = SaveInfo.getInstance();
         SaveInfo saveInfo2 = SaveInfo.getInstance();
         System.out.println("saveInfo1" + saveInfo1);
         System.out.println("saveInfo2" + saveInfo2);
 
-
         assertEquals(saveInfo1.toString(), saveInfo2.toString());
 
     }
 
-    @Test
-    public void TestSingletonIfNotNull(){
+    @Test public void TestSingletonIfNotNull()
+    {
 
-        SaveInfo.getInstance().setFlights(new Flights("flightID", "flightName", new Depature(1,"departure","depatureDate"),new Arrival(1,"arrival","arrivaldate"),new PlaneType(1,"planeType"),"price"));
+        SaveInfo.getInstance().setFlights(new Flights("flightID", "flightName",
+            new Depature(1, "departure", "depatureDate"),
+            new Arrival(1, "arrival", "arrivaldate"),
+            new PlaneType(1, "planeType"), "price"));
         Flights flights = SaveInfo.getInstance().getFlights();
-        Flights flight = new Flights("flightID", "flightName", new Depature(1,"departure","depatureDate"),new Arrival(1,"arrival","arrivaldate"),new PlaneType(1,"planeType"),"price");
+        Flights flight = new Flights("flightID", "flightName",
+            new Depature(1, "departure", "depatureDate"),
+            new Arrival(1, "arrival", "arrivaldate"),
+            new PlaneType(1, "planeType"), "price");
 
-        assertEquals(flights.toString(),flight.toString());
+        assertEquals(flights.toString(), flight.toString());
     }
-
-    //virker ikke
-    @Test
-    public void TestSingletonException(){
-
-      //  SaveInfo.getInstance().setFlights(new Flights("flightID", "flightName","200"));
-        //Flights flights = SaveInfo.getInstance().getFlights();
-      //  Flights flight = new Flights("flightID", "flightName","200");
-
-        assertThrows(NullPointerException.class,() -> SaveInfo.getInstance().getFlights());
-
-    }
-
 }
--- test/client/views/dashboard/dashboardViewModelTest.java
+++ test/client/views/dashboard/dashboardViewModelTest.java
@@ -32,14 +32,46 @@ class dashboardViewModelTest
 
   @Test void testNoSelectedFlight()
   {
+    Flights flights = SaveInfo.getInstance().getFlights();
+    viewModel.getFlightInformation(flights);
+    assertEquals("Please choose a flight for continue ",viewModel.errorProperty().get());
 
-    SaveInfo.getInstance().setFlights(new Flights("","",new Depature(0,"",""),new Arrival(0,"","")
-        ,new PlaneType(0,""),""));
+
+  }
+
+  @Test void testSelectedFlight()
+  {
+    SaveInfo.getInstance().setFlights(new Flights("1","sas",new Depature(1,"KBH","2021-08-08 19:30:00"),new Arrival(1,"AAR","2021-09-08 19:30:00")
+        ,new PlaneType(1,"Airbus7"),"200"));
+    Flights flights = SaveInfo.getInstance().getFlights();
+    viewModel.getFlightInformation(flights);
+
+    assertNull(viewModel.errorProperty().get());
+  }
+
+
+  @Test void testSearchFlightIfValidInput(){
+
+    SaveInfo.getInstance().setFlights(new Flights("1","sas",new Depature(1,"KBH","2021-08-08 19:30:00"),new Arrival(1,"AAR","2021-09-08 19:30:00")
+        ,new PlaneType(1,"Airbus7"),"200"));
     SaveInfo.getInstance().getFlights();
-    viewModel.getFlightInformation()
-    assertEquals("",viewModel.errorProperty().get());
+    viewModel.loadFlights();
+    viewModel.searchProperty().setValue("sas");
+    viewModel.search();
+    assertEquals("",viewModel.getflight());
+  }
 
 
+
+  @Test void testSearchFlightIfInvalidInput(){
+
+    SaveInfo.getInstance().setFlights(new Flights("1","sas",new Depature(1,"KBH","2021-08-08 19:30:00"),new Arrival(1,"AAR","2021-09-08 19:30:00")
+        ,new PlaneType(1,"Airbus7"),"200"));
+    SaveInfo.getInstance().getFlights();
+    viewModel.loadFlights();
+    viewModel.searchProperty().setValue("ÅÅÅÅÅ");
+    viewModel.search();
+    assertEquals("",viewModel.getflight());
   }
 
 
